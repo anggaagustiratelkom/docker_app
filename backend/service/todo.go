e92e3a33ef29ae9b6b5e7881b913df2534029477
anggaagustira <anggaagustira@gmail.com>
2021-12-20 15:42:05 +0700

    ganti backend

--- backend/service/todo.go
+++ backend/service/todo.go
@@ -3,8 +3,8 @@ package service
 import (
 	"context"
 
-	"github.com/girikuncoro/belajar-docker-pemula/backend/db"
-	"github.com/girikuncoro/belajar-docker-pemula/backend/schema"
+	"github.com/anggaagustiratelkom/docker_app/backend/db"
+	"github.com/anggaagustiratelkom/docker_app/backend/schema"
 )
 
 func GetAll(ctx context.Context) ([]schema.Todo, error) {
